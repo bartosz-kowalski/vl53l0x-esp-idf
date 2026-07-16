1f2cf99858b1aa9d30cb12a5ec65bfa02e1a30bb
Bartosz Kowalski <158082139+bartosz-kowalski@users.noreply.github.com>
2026-07-16 15:49:57 +0200

    Added clock source configuration to I2C

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -11,21 +11,22 @@ void _i2c::init(int scl, int sda) {
     const int I2C_MASTER_FREQ_HZ = 100000;   /*!< I2C master clock frequency */
 
     i2c_master_bus_config_t bus_config = {};
-    bus_config.i2c_port = I2C_MASTER_NUM,
-    bus_config.sda_io_num = I2C_MASTER_SDA_IO,
-    bus_config.scl_io_num = I2C_MASTER_SCL_IO,
-    bus_config.glitch_ignore_cnt = 7,
-    bus_config.intr_priority = 0,
-    bus_config.trans_queue_depth = 0,
+    bus_config.i2c_port = I2C_MASTER_NUM;
+    bus_config.sda_io_num = I2C_MASTER_SDA_IO;
+    bus_config.scl_io_num = I2C_MASTER_SCL_IO;
+    bus_config.clk_source = I2C_CLK_SRC_DEFAULT;
+    bus_config.glitch_ignore_cnt = 7;
+    bus_config.intr_priority = 0;
+    bus_config.trans_queue_depth = 0;
     bus_config.flags = {
         .enable_internal_pullup = true,
         .allow_pd = false,};
 
     i2c_device_config_t device_config = {};
-    device_config.dev_addr_length = I2C_ADDR_BIT_LEN_7,
-    device_config.device_address = 0x29,
-    device_config.scl_speed_hz = I2C_MASTER_FREQ_HZ,
-    device_config.scl_wait_us = 0,
+    device_config.dev_addr_length = I2C_ADDR_BIT_LEN_7;
+    device_config.device_address = 0x29;
+    device_config.scl_speed_hz = I2C_MASTER_FREQ_HZ;
+    device_config.scl_wait_us = 0;
     device_config.flags = {
         .disable_ack_check = false,};
 
